aec27cdb07b556dbda26c1c6659328dbbadd2414
hassan <hassanali7303@gmail.com>
2023-06-25 00:28:44 +0330

    add axios.get for send token

--- src/component/gmail/Gmail.jsx
+++ src/component/gmail/Gmail.jsx
@@ -1,4 +1,12 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
 export function Gmail() {
+  const { code } = useParams();
+  async function sendTocen() {
+    const res = await axios.get("", { code });
+    console.log(res);
+  }
+  sendTocen();
   return (
     <>
       <div style={{ background: "black" }}></div>
